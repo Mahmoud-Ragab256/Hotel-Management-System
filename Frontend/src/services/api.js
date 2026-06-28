168c59bbaad1e35739694f0ce703ebeddd21ff7e
Mahmoud Ragab <sendbad20006@gmail.com>
2026-06-29 01:29:07 +0300

    edit api link to link with production mode

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { getAuthToken } from './auth.js';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://hotel-management-system-sigma-ruby.vercel.app',
   timeout: 15000
 });
 
